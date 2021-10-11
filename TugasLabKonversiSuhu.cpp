624740affd4fd13860bdf1d4ac0883b3e861ab23
Paksi Ringkang <47846634+Latorynska@users.noreply.github.com>
2021-10-11 16:07:37 +0700

    Update TugasLabKonversiSuhu.cpp

--- TugasLabKonversiSuhu.cpp
+++ TugasLabKonversiSuhu.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 main(){
 	//deklarasi variabel celsius
-	int celsius;
+	float celsius;
 	
 	cout<<"Masukkan suhu celsius : "; cin>>celsius;//proses input suhu celsius
 	cout<<"Suhu dalam satuan fahrenheit : "<< celsius * 1.8 + 32; //hasil langsung di outputkan, tidak perlu dimasukkan ke variabel mana pun
